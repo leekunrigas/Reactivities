751b041126422294c57bd576153fada37c89f5f0
RigasLitsas <rigas95@gmail.com>
2020-11-09 14:04:19 +0200

    form submit && validation

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,5 +1,7 @@
 import { makeObservable, action, observable, computed, configure, runInAction } from "mobx";
 import { createContext, SyntheticEvent } from "react";
+import { toast } from "react-toastify";
+import { history } from "../..";
 import agent from "../api/agent";
 import { IActivity } from "../models/activity";
 
@@ -21,10 +23,10 @@ class ActivityStore {
   }
 
   groupActivitiesByDate(activities: IActivity[]) {
-    const sortedActivities = activities.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+    const sortedActivities = activities.sort((a, b) => a.date.getTime() - b.date.getTime());
     return Object.entries(
       sortedActivities.reduce((activities, activity) => {
-        const date = activity.date.split("T")[0];
+        const date = activity.date.toISOString().split("T")[0];
         activities[date] = activities[date] ? [...activities[date], activity] : [activity];
         return activities;
       }, {} as { [key: string]: IActivity[] })
@@ -37,7 +39,7 @@ class ActivityStore {
       const activities = await agent.Activities.list();
       runInAction(() => {
         activities.forEach((activity) => {
-          activity.date = activity.date.split(".")[0];
+          activity.date = new Date(activity.date);
           this.activityRegistry.set(activity.id, activity);
         });
       });
@@ -54,19 +56,24 @@ class ActivityStore {
     let activity = this.getActivity(id);
     if (activity) {
       this.activity = activity;
+      return activity;
     } else {
       this.loadingInitial = true;
       try {
         activity = await agent.Activities.details(id);
         runInAction(() => {
+          activity.date = new Date(activity.date);
           this.activity = activity;
+          this.activityRegistry.set(activity.id, activity);
+          this.loadingInitial = false;
         });
+        return activity;
       } catch (error) {
         console.log(error);
+        runInAction(() => {
+          this.loadingInitial = false;
+        });
       }
-      runInAction(() => {
-        this.loadingInitial = false;
-      });
     }
   };
 
@@ -85,8 +92,10 @@ class ActivityStore {
       runInAction(() => {
         this.activityRegistry.set(activity.id, activity);
       });
+      history.push(`/activities/${activity.id}`);
     } catch (error) {
-      console.log(error);
+      console.log(error.response);
+      toast.error("Problem submitting data");
     }
     runInAction(() => {
       this.submitting = false;
@@ -101,8 +110,10 @@ class ActivityStore {
         this.activityRegistry.set(activity.id, activity);
         this.activity = activity;
       });
+      history.push(`/activities/${activity.id}`);
     } catch (error) {
-      console.log(error);
+      console.log(error.repsonse);
+      toast.error("Problem submitting data");
     }
     runInAction(() => {
       this.submitting = false;
